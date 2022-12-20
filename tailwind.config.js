ecba8fbc0abda3d764d2c82ea747ec9a9d3c6d3f
el shamiso <tapiwamusinga1@gmail.com>
2022-12-20 21:09:44 +0800

    added content to site

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     extend: {
       fontFamily: {
         'nunito': ['Nunito']
-      }
+      },
+      colors: {
+        custom: {
+        'primary-brand': '#ab755b',
+        'secondary-brand' :'#bc976c',
+        'tertiary-brand' :'#7f4e25'
+        }
+      },
     },
   },
   plugins: [],
